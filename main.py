f91869c85ccea1e10a8347603a2a73b364941ade
Kajot-dev <83967@student.ue.poznan.pl>
2024-04-17 10:51:07 +0200

    Add waluta_dict_na_str function
    Signed-off-by: Kajot-dev <83697@student.ue.poznan.pl>

--- main.py
+++ main.py
@@ -12,3 +12,33 @@ def wyslij_sowe(adresat, tresc):
         print("Dostarczenie sowy niemożliwe")
         return False
 
+def waluta_dict_na_str(fundusz: dict[str, int]) -> str:
+    # nie zmieniamy kolejności walut w słowniku
+    buffer = []
+    for waluta, ilosc in fundusz.items():
+        
+        if ilosc == 0:
+            continue
+        
+        last_digit = ilosc
+
+        while last_digit >= 10:
+            last_digit = last_digit % 10
+            
+        if last_digit > 1 and last_digit < 5:
+            match waluta[-1]:
+                case "l":
+                    waluta += "e"
+                case _:
+                    waluta += "y"
+
+        elif last_digit >= 5:
+            match waluta[-1]:
+                case "l":
+                    waluta += "i"
+                case _:
+                    waluta += "ów"
+            
+        buffer.append(f"{ilosc} {waluta}")
+        
+    return " ".join(buffer)
