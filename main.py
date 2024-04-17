850d19798adf64c5802eea642a2b316fc4ae92ae
Hubert Lagoda <lagodahubert.@gmail.com>
2024-04-17 10:55:25 +0200

    dodano zadanie 4

--- main.py
+++ main.py
@@ -11,6 +11,23 @@ def wyslij_sowe(adresat, tresc):
     else:
         print("Dostarczenie sowy niemożliwe")
         return False
+    
+def wybierz_sowe_zwroc_koszt(odbior, odleglosc, typ, specjalna):
+    koszt = {"galeon": 0, "sykl": 0, "knut": 0}
+    koszt['knut'] += 7 if odbior else 0
+    koszt['knut'] += 4 if specjalna == "wyjec" else 1 if specjalna == 'list gonczy' else 0
+
+    # [knut, sykl]
+    odleglosc_koszt = {
+        "lokalna": {"list": [2, 0], "paczka": [7, 0]},
+        "krajowa": {"list": [12, 0], "paczka": [2, 1]},
+        "dalekobiezna": {"list": [20, 0], "paczka": [1, 2]}
+    }
+
+    koszt['knut'] += odleglosc_koszt[odleglosc][typ][0]
+    koszt['sykl'] += odleglosc_koszt[odleglosc][typ][1]
+
+    return koszt
 
 def waluta_dict_na_str(fundusz: dict[str, int]) -> str:
     # nie zmieniamy kolejności walut w słowniku
@@ -42,3 +59,6 @@ def waluta_dict_na_str(fundusz: dict[str, int]) -> str:
         buffer.append(f"{ilosc} {waluta}")
         
     return " ".join(buffer)
+
+
+
