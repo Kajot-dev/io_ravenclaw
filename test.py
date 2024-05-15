f2f0069059eb15aeddc80e6ac284bf805916a008
Przemyslaw Kula <83509@student.ue.poznan.pl>
2024-05-15 13:15:47 +0200

    final fixing ex 3

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from main import * 
+import main 
 
 def test_wybierz_sowe_zwroc_koszt(odbior: bool, odleglosc, typ, specjalna, wynik):
     
@@ -52,7 +52,7 @@ def test_licz_sume():
         "sykl" : [18, 20, 10],
         "knut" : [30, 40, 7]
     }
-    wynik = licz_sume(fundusz)
+    wynik = main.licz_sume(fundusz)
     assert wynik == {
         "galeon" : 12,
         "sykl" : 0,
@@ -64,7 +64,7 @@ def test_licz_sume():
         "sykl" : [],
         "knut" : []
     }
-    wynik = licz_sume(fundusz)
+    wynik = main.licz_sume(fundusz)
     assert wynik == {
         "galeon" : 0,
         "sykl" : 0,
@@ -76,7 +76,7 @@ def test_licz_sume():
         "sykl" : [0, 100, 0],
         "knut" : [0, 0, 100]
     }
-    wynik = licz_sume(fundusz)
+    wynik = main.licz_sume(fundusz)
     assert wynik == {
         "galeon" : 106,
         "sykl" : 2,
