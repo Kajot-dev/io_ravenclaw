70208a467b10a67f525bccf6c309cc9c574bbddd
Przemyslaw Kula <83509@student.ue.poznan.pl>
2024-05-15 11:25:29 +0200

    dodano test do zadania 3

--- test.py
+++ test.py
@@ -1,1 +1,41 @@
-import main
+from main import licz_sume
+
+def test_licz_sume():
+    fundusz = {
+        "galeon" : [1, 3, 5],
+        "sykl" : [18, 20, 10],
+        "knut" : [30, 40, 7]
+    }
+    wynik = licz_sume(fundusz)
+    assert wynik == {
+        "galeon" : 12,
+        "sykl" : 0,
+        "knut" : 14
+    }
+
+    fundusz = {
+        "galeon" : [],
+        "sykl" : [],
+        "knut" : []
+    }
+    wynik = licz_sume(fundusz)
+    assert wynik == {
+        "galeon" : 0,
+        "sykl" : 0,
+        "knut" : 0
+    }
+
+    fundusz = {
+        "galeon" : [100, 0, 0],
+        "sykl" : [0, 100, 0],
+        "knut" : [0, 0, 100]
+    }
+    wynik = licz_sume(fundusz)
+    assert wynik == {
+        "galeon" : 106,
+        "sykl" : 2,
+        "knut" : 16
+    }
+test_licz_sume()
+
+
