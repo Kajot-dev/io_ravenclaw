e86eee4f6ee5843a15bde818eb7724cb5e4dc4d4
Mikolaj Owsianny <83515@student.ue.poznan.pl>
2024-05-15 13:02:39 +0200

    Fixed tests for task 2

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import main
+from unittest.mock import patch
 
 def test_wyslij_sowe():
     liczba_sukcesow = 0
     liczba_prob = 10000
     
-    for _ in range(liczba_prob):
-        if wyslij_sowe("Harry", "Wiadomosc"):
-            liczba_sukcesow += 1
+    with patch('main.time.sleep', return_value=None):
+        for _ in range(liczba_prob):
+            if main.wyslij_sowe("Harry", "Wiadomosc"):
+                liczba_sukcesow += 1
     
     wspolczynnik_sukcesow = liczba_sukcesow / liczba_prob
     
     # Sprawdzanie, czy sukces pojawia się w okolicach 85%
     assert 0.80 <= wspolczynnik_sukcesow <= 0.90
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     test_wyslij_sowe()
-
